e2ea82f6611e499b74aae9c2142b3832b2cbe5bc
Marius Nielsen <mawn@itu.dk>
2023-09-07 15:44:49 +0200

    now uses docopt for cmdline args

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Chirp.CLI.Shared;
 using Chirp.CLI.Types;
 using Chirp.SimpleDB.Storage;
+using DocoptNet;
+
 
 namespace Chirp.CLI;
 public static class Program
@@ -8,31 +10,39 @@ public static class Program
     private static IStorage<ChirpMessage>? _csvStorage;
     public static void Main(string[] args)
     {
-        if (args.Length == 0)
-        {
-            Console.WriteLine("I need arguments!!!!");
-            return;
-        }
-        
+
+        const string USAGE = @"
+Usage:
+    Chirl.CLI read
+    Chirl.CLI cheep <message>
+
+Options:
+    -h --help     Show this screen.
+";
+
+        var arguments = new Docopt().Apply(USAGE, args, exit: true);
+
         _csvStorage = CsvStorageProvider<ChirpMessage>.Storage;
         
-
-        switch (args[0])
+        if (arguments["cheep"].IsTrue)
         {
-            case "cheep":
-                var message = new ChirpMessage(
-                    Environment.UserName,
-                    args[1],
-                    DateTimeHelper.DateTimeToEpoch(DateTime.Now));
-                _csvStorage.StoreEntity(message);
-                break;
-            case "read":
-                var records = _csvStorage.GetEntities();
-                foreach (var chirpMessage in records)
-                {
-                    Console.WriteLine(chirpMessage.ToString());
-                }
-                break;
+            var message = new ChirpMessage(
+                Environment.UserName,
+                args[1],
+                DateTimeHelper.DateTimeToEpoch(DateTime.Now));
+            _csvStorage.StoreEntity(message);
+        }
+        else if(arguments["read"].IsTrue)
+        {
+            var records = _csvStorage.GetEntities();
+            foreach (var chirpMessage in records)
+            {
+                Console.WriteLine(chirpMessage.ToString());
+            }
+        }
+        else if (arguments["--help"].IsTrue)
+        {
+            Console.WriteLine(USAGE);
         }
     }
 }
