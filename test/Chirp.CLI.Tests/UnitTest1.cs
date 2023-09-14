06d119d3466573d1f9f6d4a404da4fe0d3f6da63
Juules32 <Juules32@gmail.com>
2023-09-14 12:55:31 +0200

    separated into src and test

---
+++ test/Chirp.CLI.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace Chirp.CLI.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        //Assert
+        int a = 1;
+
+        //Act
+        a++;
+
+        //Assert
+        Assert.True(a == 2);
+    }
+}
