315e131d7ad2e6efb98d6ca64aba3cb5d20eddf1
Marius Nielsen <mawn@itu.dk>
2023-09-14 14:36:17 +0200

    testing building and testing

--- test/Chirp.SimpleDB.Tests/UnitTest1.cs
+++ test/Chirp.SimpleDB.Tests/UnitTest1.cs
@@ -5,6 +5,13 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        //Assert
+        int a = 1;
 
+        //Act
+        a++;
+
+        //Assert
+        Assert.True(a == 2);
     }
 }
