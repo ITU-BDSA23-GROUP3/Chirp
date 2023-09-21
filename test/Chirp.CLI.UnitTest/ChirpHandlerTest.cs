29f5a49bed982c7ea966648515bf466b380328fd
alrj <alrj@itu.dk>
2023-09-21 16:21:40 +0200

    Test updated

--- test/Chirp.CLI.UnitTest/ChirpHandlerTest.cs
+++ test/Chirp.CLI.UnitTest/ChirpHandlerTest.cs
@@ -8,27 +8,34 @@ namespace Chirp.CLI.UnitTest;
 
 public class ChirpHandlerTest
 {
-    private readonly IStorageProvider<ChirpRecord> _storage = Substitute.For<IStorageProvider<ChirpRecord>>();
-    private readonly IStorage<ChirpRecord> _providedStorage = Substitute.For<IStorage<ChirpRecord>>();
+    private readonly IHttpServiceProvider _service = Substitute.For<IHttpServiceProvider>();
+    private readonly HttpClient _client = Substitute.For<HttpClient>();
     private readonly IArgumentsProvider _argsProvider = Substitute.For<IArgumentsProvider>();
     private readonly IUserInterface _ui = Substitute.For<IUserInterface>();
     private readonly ChirpHandler _sut;
 
     public ChirpHandlerTest()
     {
-        _sut = new ChirpHandler(_storage, _argsProvider,_ui);
+        _sut = new ChirpHandler(_service, _argsProvider,_ui);
+        _client.BaseAddress = new Uri("http://haha");
     }
 
     [Fact]
     public void ChirpHandler_ProvidesArgument_ArgumentsReadAndExecuted()
     {
-        _storage.Storage.Returns(_providedStorage);
+        // Arrange
+        _service.Client.Returns(_client);
         var argDict = new Dictionary<string, ArgValue>
         {
             { "read", ArgValue.True },
             { "cheep", ArgValue.False },
             { "--help", ArgValue.False }
         };
+        
+        // Act
         _sut.HandleCustomArgs(argDict);
+        
+        // Assert
+        _client.Received().GetAsync("/Chirp");
     }
 }
