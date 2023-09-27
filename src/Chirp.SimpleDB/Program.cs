89002d06bb27637407a353122e81eb5750f50292
alrj <alrj@itu.dk>
2023-09-27 12:54:08 +0200

    always use swagger

--- src/Chirp.SimpleDB/Program.cs
+++ src/Chirp.SimpleDB/Program.cs
@@ -25,11 +25,11 @@ app.UseCors(builder => builder
     .AllowAnyHeader());
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+// }
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
