e1182462ccd50831a4fda8f490751655c4a6bebf
Benjamin Jensen <Benjaminbyvej@gmail.com>
2023-09-28 15:24:46 +0200

    divided into src test

--- src/Chirp.Razor/CheepService.cs
+++ src/Chirp.Razor/CheepService.cs
@@ -1,3 +1,5 @@
+namespace Chirp.Razor;
+
 public record CheepViewModel(string Author, string Message, string Timestamp);
 
 public interface ICheepService
--- src/Chirp.Razor/Program.cs
+++ src/Chirp.Razor/Program.cs
@@ -1,3 +1,5 @@
+using Chirp.Razor;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
