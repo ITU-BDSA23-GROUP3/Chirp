8f79696dd9f3fdab66a2f2cae366e0d3976f4b77
NatalieCP <natp@itu.dk>
2023-09-28 16:48:02 +0200

    SQLites3 implemented in CheepService (#44)

--- src/Chirp.Razor/CheepService.cs
+++ src/Chirp.Razor/CheepService.cs
@@ -1,4 +1,6 @@
 namespace Chirp.Razor;
+using System.Data;
+using Microsoft.Data.Sqlite;
 
 public record CheepViewModel(string Author, string Message, string Timestamp);
 
@@ -11,14 +13,32 @@ public interface ICheepService
 public class CheepService : ICheepService
 {
     // These would normally be loaded from a database for example
-    private static readonly List<CheepViewModel> _cheeps = new()
-        {
-            new CheepViewModel("Helge", "Hello, BDSA students!", UnixTimeStampToDateTimeString(1690892208)),
-            new CheepViewModel("Rasmus", "Hej, velkommen til kurset.", UnixTimeStampToDateTimeString(1690895308)),
-        };
+    private static readonly List<CheepViewModel> _cheeps = new List<CheepViewModel>();
 
     public List<CheepViewModel> GetCheeps()
     {
+        var sqlDBFilePath = "/tmp/chirp.db";
+        var sqlQuery = @"SELECT user.username, message.text, message.pub_date FROM message JOIN user ON user.user_id = message.author_id ORDER by message.pub_date desc";
+
+        using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
+        {
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText = sqlQuery;
+
+            using var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                var dataRecord = (IDataRecord)reader;
+                
+                var author = dataRecord[0].ToString();
+                var message = dataRecord[1].ToString();
+                var timestamp = UnixTimeStampToDateTimeString(double.Parse(dataRecord[2].ToString()));
+
+                _cheeps.Add(new CheepViewModel(author,message,timestamp));
+            }
+        }
         return _cheeps;
     }
 
