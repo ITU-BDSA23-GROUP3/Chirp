6c2dd5f1919ab712029298f4d67709ec41ba5be1
Benjamin Jensen <Benjaminbyvej@gmail.com>
2023-11-09 15:53:44 +0100

    changed import

--- src/Chirp.Core/Enitities.cs
+++ src/Chirp.Core/Enitities.cs
@@ -1,4 +1,4 @@
-using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace Chirp.Core;
 
@@ -15,7 +15,7 @@ public class Cheep
     public int CheepId { get; set; }
     public int AuthorId { get; set; }
     public Author Author { get; set; }
-    
+
     [MaxLength(180)]
     public required string Text { get; set; }
     public required DateTime TimeStamp { get; set; }
