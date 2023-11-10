fff33cd89c7bb5396d84e5914cf7b4d2bf3e164d
Benjamin Jensen <Benjaminbyvej@gmail.com>
2023-11-10 21:38:27 +0100

    added option to reset the contents of the tables

---
+++ src/Chirp.Web/Pages/ResetTables.cshtml.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Chirp.Infrastructure;
+namespace Chirp.Web.Pages;
+
+public class ResetTables : PageModel
+{
+
+    private ChirpDBContext _db;
+    public ResetTables(ChirpDBContext db)
+    {
+        _db = db;
+    }
+    public IActionResult OnPost()
+    {
+        _db.Cheeps.RemoveRange(_db.Cheeps);
+        _db.Authors.RemoveRange(_db.Authors);
+        _db.SaveChanges();
+
+        DbInitializer.SeedDatabase(_db);
+
+        return RedirectToPage("Public");
+    }
+}
