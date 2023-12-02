6c50831022918ad960dbdd05b38c5591692088ed
Natalie Clara Petersen <natp@itu.dk>
2023-12-02 15:27:51 +0100

    Requested changes repository manager and user information page
    Co-authored-by: Juules32 <benjaminbyvej@gmail.com>

--- src/Chirp.Infrastructure/IRepositoryManager.cs
+++ src/Chirp.Infrastructure/IRepositoryManager.cs
@@ -25,54 +25,34 @@ public class RepositoryManager : IRepositoryManager
     }
 
     public IAuthorRepository AuthorRepository
-    {
-        get
+    { 
+        get 
         {
-            if (_authorRepository == null)
-            {
-                _authorRepository = new AuthorRepository(_db);
-            }
-
-            return _authorRepository;
+            return _authorRepository ??= new AuthorRepository(_db);
         }
     }
 
-    public ICheepRepository CheepRepository
+    public IFollowRepository FollowRepository 
     {
-        get
+        get 
         {
-            if (_cheepRepository == null)
-            {
-                _cheepRepository = new CheepRepository(_db, FollowRepository, AuthorRepository);
-            }
-
-            return _cheepRepository;
+            return _followRepository ??= new FollowRepository(_db);
         }
     }
 
-    public IFollowRepository FollowRepository
+    public ICheepRepository CheepRepository 
     {
-        get
+        get 
         {
-            if (_followRepository == null)
-            {
-                _followRepository = new FollowRepository(_db);
-            }
-
-            return _followRepository;
+            return _cheepRepository ??= new CheepRepository(_db, FollowRepository, AuthorRepository);
         }
     }
-
-    public ILikeRepository LikeRepository
+    
+    public ILikeRepository LikeRepository 
     {
         get
         {
-            if (_likeRepository == null)
-            {
-                _likeRepository = new LikeRepository(_db);
-            }
-
-            return _likeRepository;
+            return _likeRepository ??= new LikeRepository(_db);
         }
     }
 }
